4d59164a7ceda9fda00dd792f8c1e6cabeb6abf0
liuyi34 <vic.liu@mail.utoronto.ca>
2017-04-24 22:19:38 -0400

    commit point.c

--- warmup/point.c
+++ warmup/point.c
@@ -1,23 +1,32 @@
 #include <assert.h>
 #include "common.h"
 #include "point.h"
+#include <math.h>
 
 void
 point_translate(struct point *p, double x, double y)
 {
-	TBD();
+    p->x += x; 
+    p->y += y;
+    return;
 }
 
 double
 point_distance(const struct point *p1, const struct point *p2)
 {
-	TBD();
-	return -1.0;
+    
+    return sqrt(((p2->x - p1->x)*(p2->x - p1->x)) + ((p2->y - p1->y) * (p2->y - p1->y)));
 }
 
 int
 point_compare(const struct point *p1, const struct point *p2)
 {
-	TBD();
-	return 0;
+    double l1 = ((p1->x)*(p1->x)+(p1->y)*(p1->y));
+    double l2 = ((p2->x)*(p2->x)+(p2->y)*(p2->y));
+    if(l1 < l2)
+        return -1;
+    else if(l1 == l2)
+        return 0;
+    else
+        return 1;
 }
